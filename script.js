25a43da871b980c92d453b4eb19c137a94d29c55
Nitin <nitingarhwal184@gmail.com>
2026-01-06 20:02:46 +0530

    Day 1: JavaScript basics and console output

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Hello Javascript");
+console.log("My name is Nitin");
+console.log(10 + 20);
+console.log("I will become a Web Developer");
+console.log(100-50);
+console.log(5 * 8);
+
+
+console.log("Day 1 Completed Successfully ");
+console.log(20/5);
